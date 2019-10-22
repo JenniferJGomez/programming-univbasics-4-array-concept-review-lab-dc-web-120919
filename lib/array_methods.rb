0032beb0bbfeeb2387e6bdcfa6a9be260fdbf4a2
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-10-22 16:52:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -17,5 +17,13 @@ def find_max_value(array)
   x
 end
 
-
+def find_min_value(array)
+  x = array[0]
+  array.length.times {|index|
+    if array[index] < x
+      x = array[index]
+    end
+  }
+  x
+end
 
